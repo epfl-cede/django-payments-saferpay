eadd2648223e9c42dd7403bb101ca9ca77dd9da1
Oleg Demakov <oleg.demakov@epfl.ch>
2025-04-17 14:50:56 +0200

    add github action for tests

--- src/django_payments_saferpay/version.py
+++ src/django_payments_saferpay/version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.0.post9+g454d333.d20250417"
-__version_tuple__ = version_tuple = (0, 0, "g454d333.d20250417")
+__version__ = version = "0.0.post10+g03884fb.d20250417"
+__version_tuple__ = version_tuple = (0, 0, "g03884fb.d20250417")
